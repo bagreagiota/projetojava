77e4dc207858b6c8087cfa3d32fbd6a93a1e70bd
bagreagiota <113524247+bagreagiota@users.noreply.github.com>
2023-10-02 07:47:54 -0300

    Add files via upload

--- gabs/media.js
+++ gabs/media.js
@@ -1,22 +1,24 @@
-//Cria referencência ao form e elementos de resposta do programa
+//Cria referência ao form e elementos de resposta do programa
 
 const form = document.querySelector("form")
 const resp1 = document.querySelector("h3")
 const resp2 = document.querySelector("h4")
 
-//cria um ouvinte
+//Cria um ouvinte
 
-form.addEventListener("submit",(e) =>{
-    e.preventDefault()               //evita o envio do form
+form.addEventListener("subimit",(e) =>{ 
+    e.preventDefault() //evita o envio do form
 
-const nome = form.inNome.value           //obtém o nome
-const nota1 = Number(form.inNota1.value) //obtém a primeira nota
-const nota2 = Number(form.inNota2.value) //obtém a segunda nota
-const media = (nota1 + nota2)/2
-resp1.innerText = `Média das Notas  $(media.toFixed(2)}
+    const nome = form.inNone.value    //obtem o nome
+    const nota1 = Number(form.inNota1.value)   //obtem a primeira nota
+    const nota2 = Number(form.inNota2.value)    //obtem a segunda nota
+    const media = (nota1 + nota2)/2  //media das notas
+    resp1.innerText =  `Media das Notas ${media.toFixed(2)}`
 
+    //cria as condicionais
 
-
-
-}
-)
+    if(media >=7) {
+        resp2.innerText =`Parabéns ${nome}! Voce foi aprovado(a)`
+        resp2
+    }
+})
